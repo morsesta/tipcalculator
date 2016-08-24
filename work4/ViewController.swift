2d5c0b466abff81c37fdda74ade711254c2a584b
Yun Chu Wang <morsesta@yunde-macbook-pro.local>
2016-08-24 11:59:59 +0800

    Initial Commit

---
+++ work4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  work4
+//
+//  Created by Yun Chu Wang on 2016/8/24.
+//  Copyright © 2016年 Yun Chu Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
