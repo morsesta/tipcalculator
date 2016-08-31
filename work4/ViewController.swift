5247a7bb8766898005b2e0478f3ad057c56056a7
Yun Chu Wang <morsesta@yunde-macbook-pro.local>
2016-08-31 14:25:50 +0800

    update Tip calculator

--- work4/ViewController.swift
+++ work4/ViewController.swift
@@ -9,17 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func calculate(_ sender: AnyObject) {
+        let tipPercent = Double(tipTextField.text!)
+        let price = Double(priceTextField.text!)
+        if let price = price {
+            if let tipPercent = tipPercent {
+                let tip = price * tipPercent
+                tipLabel.text = "\(tip)"
+            }
+        }
+        
+    }
 
+    
+    
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var tipTextField: UITextField!
+    
+    @IBOutlet weak var priceTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
+    
+    
+    
+    
+
 
